0adc905e8d44f62f0c4ac5e34e001e5fcd75a3dc
Michael Becker <michael.f.becker@gmail.com>
2016-07-03 21:32:27 -0400

    Fixing Demo directory names

---
+++ Linux/Demo/null_g++/main.cpp
@@ -0,0 +1,54 @@
+/****************************************************************************
+ *
+ *  Copyright (c) 2016, Michael Becker (michael.f.becker@gmail.com)
+ *
+ *  This file is part of the FreeRTOS C++ Wrappers project.
+ *
+ *  The FreeRTOS C++ Wrappers project is free software: you can redistribute
+ *  it and/or modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  The FreeRTOS C++ Wrappers project is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the FreeRTOS C++ Wrappers project.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+int main (void)
+{
+    return 0;
+}
+
+
+void vAssertCalled( unsigned long ulLine, const char * const pcFileName )
+{
+    printf("ASSERT: %s : %d\n", pcFileName, (int)ulLine);
+    while (1);
+}
+
+
+unsigned long ulGetRunTimeCounterValue(void)
+{
+    return 0;
+}
+
+void vConfigureTimerForRunTimeStats(void)
+{
+    return;
+}
+
+
+extern "C" void vApplicationMallocFailedHook(void);
+void vApplicationMallocFailedHook(void)
+{
+	while(1);
+}
+
