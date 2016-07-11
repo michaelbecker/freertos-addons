40ea57c96f453cefce7cca90ac2c5e1d598a89f0
Michael Becker <michael.f.becker@gmail.com>
2016-07-10 21:19:35 -0400

    Starting work on semaphore testing

---
+++ Linux/Demo/Linux_g++_binary_semaphore/main.cpp
@@ -0,0 +1,113 @@
+/****************************************************************************
+ *
+ *  Copyright (c) 2016, Michael Becker (michael.f.becker@gmail.com)
+ *
+ *  This file is part of the FreeRTOS C++ Wrappers project.
+ *
+ *  The FreeRTOS C++ Wrappers project is free software: you can redistribute
+ *  it and/or modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, version 3 of the License.
+ *
+ *  The FreeRTOS C++ Wrappers project is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the FreeRTOS C++ Wrappers project.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+#include <stdio.h>
+#include <iostream>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "thread.hpp"
+#include "ticks.hpp"
+
+
+using namespace cpp_freertos;
+using namespace std;
+
+
+
+class TestThread : public Thread {
+
+    public:
+
+        TestThread(int i, int delayInSeconds)
+           : Thread("TestThread", 100, 1), 
+             id (i), 
+             DelayInSeconds(delayInSeconds)
+        {
+        };
+
+    protected:
+
+        virtual void Run() {
+
+            cout << "Starting thread " << id << endl;
+            
+            while (true) {
+            
+                TickType_t ticks = Ticks::SecondsToTicks(DelayInSeconds);
+            
+                if (ticks)
+                    Delay(ticks);
+            
+                cout << "Running thread " << id << endl;
+            }
+        };
+
+    private:
+        int id;
+        int DelayInSeconds;
+};
+
+
+int main (void)
+{
+    cout << "Testing FreeRTOS C++ wrappers" << endl;
+    cout << "Binary Semaphore" << endl;
+
+    TestThread thread1(2, 2);
+    TestThread thread2(3, 3);
+
+    Thread::StartScheduler();
+
+    //
+    //  We shouldn't ever get here unless someone calls 
+    //  Thread::EndScheduler()
+    //
+
+    cout << "Scheduler ended!" << endl;
+
+    return 0;
+}
+
+
+void vAssertCalled( unsigned long ulLine, const char * const pcFileName )
+{
+    printf("ASSERT: %s : %d\n", pcFileName, (int)ulLine);
+    while(1);
+}
+
+
+unsigned long ulGetRunTimeCounterValue(void)
+{
+    return 0;
+}
+
+void vConfigureTimerForRunTimeStats(void)
+{
+    return;
+}
+
+
+extern "C" void vApplicationMallocFailedHook(void);
+void vApplicationMallocFailedHook(void)
+{
+	while(1);
+}
+
+
