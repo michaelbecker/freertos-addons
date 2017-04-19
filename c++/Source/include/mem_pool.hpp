a7a91e202dab18feba36d2d352812dc431462394
Michael Becker <michael.f.becker@gmail.com>
2017-04-19 18:12:13 -0400

    Starting work on fixed size memory pools

---
+++ c++/Source/include/mem_pool.hpp
@@ -0,0 +1,108 @@
+/****************************************************************************
+ *
+ *  Copyright (c) 2017, Michael Becker (michael.f.becker@gmail.com)
+ *
+ *  This file is part of the FreeRTOS C++ Wrappers project.
+ *  
+ *  Source Code:
+ *  https://github.com/michaelbecker/freertos-addons
+ *
+ *  Project Page:
+ *  http://michaelbecker.github.io/freertos-addons/
+ *
+ *  On-line Documentation:
+ *  http://michaelbecker.github.io/freertos-addons/docs/html/index.html
+ *
+ *  The FreeRTOS C++ Wrappers project is free software: you can redistribute
+ *  it and/or modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  The FreeRTOS C++ Wrappers project is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the FreeRTOS C++ Wrappers project.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  NOTE: A modification to the GPL is included to allow you to distribute a
+ *  combined work that includes FreeRTOS C++ Wrappers project
+ *  without being obliged to provide the source
+ *  code for proprietary components.
+ * 
+ *  The FreeRTOS C++ Wrappers project GPL exception text follows:
+ * 
+ *  Any FreeRTOS C++ Wrapper *source code*, whether modified or in it's 
+ *  original release form, or whether in whole or in part, can only be 
+ *  distributed by you under the terms of the GNU General Public License plus
+ *  this exception.  An independent module is a module which is not derived
+ *  from or based on FreeRTOS C++ Wrappers project.
+ * 
+ *  Clause 1:
+ * 
+ *  Linking FreeRTOS C++ Wrappers project  with other modules is making a 
+ *  combined work based on FreeRTOS C++ Wrappers project. Thus, the terms 
+ *  and conditions of the GNU General Public License V2 cover the
+ *  whole combination.
+ * 
+ *  As a special exception, the copyright holders of FreeRTOS C++ Wrappers 
+ *  project give you permission to link FreeRTOS C++ Wrappers project with 
+ *  independent modules to produce a statically linked executable, regardless
+ *  of the license terms of these independent modules, and to copy and 
+ *  distribute the resulting executable under terms of your choice,
+ *  provided that you also meet, for each linked independent module, the 
+ *  terms and conditions of the license of that module.  An independent 
+ *  module is a module which is not derived from or based on FreeRTOS C++ 
+ *  Wrappers project.
+ * 
+ *  Clause 2:
+ * 
+ *  FreeRTOS C++ Wrappers project may not be used for any competitive or 
+ *  comparative purpose, including the publication of any form of run time 
+ *  or compile time metric, without the express permission of the copyright 
+ *  holder(s) (this is the norm within the industry and is intended to ensure
+ *  information accuracy).
+ *  
+ ***************************************************************************/
+#ifndef MEM_POOL_HPP_
+#define MEM_POOL_HPP_
+
+
+#include <list>
+#include "FreeRTOS.h"
+#include "mutex.hpp"
+
+
+namespace cpp_freertos {
+
+
+class MemoryPool {
+
+    public:
+
+        MemoryPool(int itemSize, int itemCount);
+
+        MemoryPool(int itemSize, void *preallocatedMemory);
+
+        void AddMemory(int itemCount);
+
+        void AddMemory(void *preallocatedMemory, int preallocatedMemorySize);
+
+        void *Allocate();
+
+        void Free(void *item);
+
+    private:
+
+        Mutex &Lock;
+
+        const int ItemSize;
+
+        std::list<void *>FreeItems;
+};
+
+
+#endif
+
---
+++ c++/Source/mem_pool.cpp
@@ -0,0 +1,175 @@
+/****************************************************************************
+ *
+ *  Copyright (c) 2017, Michael Becker (michael.f.becker@gmail.com)
+ *
+ *  This file is part of the FreeRTOS C++ Wrappers project.
+ *  
+ *  Source Code:
+ *  https://github.com/michaelbecker/freertos-addons
+ *
+ *  Project Page:
+ *  http://michaelbecker.github.io/freertos-addons/
+ *
+ *  On-line Documentation:
+ *  http://michaelbecker.github.io/freertos-addons/docs/html/index.html
+ *
+ *  The FreeRTOS C++ Wrappers project is free software: you can redistribute
+ *  it and/or modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  The FreeRTOS C++ Wrappers project is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the FreeRTOS C++ Wrappers project.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  NOTE: A modification to the GPL is included to allow you to distribute a
+ *  combined work that includes FreeRTOS C++ Wrappers project
+ *  without being obliged to provide the source
+ *  code for proprietary components.
+ * 
+ *  The FreeRTOS C++ Wrappers project GPL exception text follows:
+ * 
+ *  Any FreeRTOS C++ Wrapper *source code*, whether modified or in it's 
+ *  original release form, or whether in whole or in part, can only be 
+ *  distributed by you under the terms of the GNU General Public License plus
+ *  this exception.  An independent module is a module which is not derived
+ *  from or based on FreeRTOS C++ Wrappers project.
+ * 
+ *  Clause 1:
+ * 
+ *  Linking FreeRTOS C++ Wrappers project  with other modules is making a 
+ *  combined work based on FreeRTOS C++ Wrappers project. Thus, the terms 
+ *  and conditions of the GNU General Public License V2 cover the
+ *  whole combination.
+ * 
+ *  As a special exception, the copyright holders of FreeRTOS C++ Wrappers 
+ *  project give you permission to link FreeRTOS C++ Wrappers project with 
+ *  independent modules to produce a statically linked executable, regardless
+ *  of the license terms of these independent modules, and to copy and 
+ *  distribute the resulting executable under terms of your choice,
+ *  provided that you also meet, for each linked independent module, the 
+ *  terms and conditions of the license of that module.  An independent 
+ *  module is a module which is not derived from or based on FreeRTOS C++ 
+ *  Wrappers project.
+ * 
+ *  Clause 2:
+ * 
+ *  FreeRTOS C++ Wrappers project may not be used for any competitive or 
+ *  comparative purpose, including the publication of any form of run time 
+ *  or compile time metric, without the express permission of the copyright 
+ *  holder(s) (this is the norm within the industry and is intended to ensure
+ *  information accuracy).
+ *  
+ ***************************************************************************/
+
+
+#include "mem_pool.h"
+
+
+using namespace cpp_freertos;
+
+
+MemoryPool::MemoryPool(int itemSize, int itemCount)
+    : Lock(MutexStandard())
+      ItemSize(itemSize)
+{
+    unsigned char *address = (unsigned char *)malloc(ItemSize * itemCount);
+
+    if (address == NULL) {
+#ifndef CPP_FREERTOS_NO_EXCEPTIONS
+        throw MemoryPoolCreateException("Constructor");
+#else
+        configASSERT(!"MemoryPool Constructor Failed");
+#endif
+    }
+
+    for (int i = 0; i < itemCount; i++) {
+        FreeItems.push_back(address);
+        address += ItemSize;
+    }
+}
+
+
+MemoryPool::MemoryPool( int itemSize, 
+                        void *preallocatedMemory, 
+                        int preallocatedMemorySize)
+    : Lock(MutexStandard()),
+      ItemSize(itemSize)
+{
+    unsigned char *address = (unsigned char *)preallocatedMemory;
+
+    while ((preallocatedMemorySize > 0)
+            && (itemSize <= preallocatedMemorySize)) {
+
+        FreeItems.push_back(address);
+        address += ItemSize;
+        preallocatedMemorySize -= ItemSize;
+    }
+}
+
+
+void *MemoryPool::Allocate()
+{
+    LockGuard guard(Lock);
+
+    if (FreeItems.empty())
+        return NULL;
+
+    void *item = FreeItems.front();
+    FreeItems.pop_front();
+
+    return item;
+}
+
+
+void MemoryPool::Free(void *item)
+{
+    LockGuard guard(Lock);
+    FreeItems.push_back(item);
+}
+
+
+void MemoryPool::AddMemory(int itemCount)
+{
+    unsigned char *address = (unsigned char *)malloc(ItemSize * itemCount);
+
+    if (address == NULL) {
+#ifndef CPP_FREERTOS_NO_EXCEPTIONS
+        throw MemoryPoolCreateException("AddMemory");
+#else
+        configASSERT(!"MemoryPool AddMemory Failed");
+#endif
+    }
+
+    for (int i = 0; i < itemCount; i++) {
+    
+        LockGuard guard(Lock);
+        
+        FreeItems.push_back(address);
+        address += ItemSize;
+    }
+}
+
+
+MemoryPool::AddMemory(  void *preallocatedMemory,
+                        int preallocatedMemorySize)
+{
+    unsigned char *address = (unsigned char *)preallocatedMemory;
+
+    while ((preallocatedMemorySize > 0)
+            && (itemSize <= preallocatedMemorySize)) {
+
+        LockGuard guard(Lock);
+        
+        FreeItems.push_back(address);
+        address += ItemSize;
+        preallocatedMemorySize -= ItemSize;
+    }
+}
+
+
